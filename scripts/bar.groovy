50f810996f348c27228ef2d62c20c8d3f884b778
Richard Maxwell <richard@rsmaxwell.co.uk>
2020-05-24 22:02:40 +0100

    implement groovy function for the transmission

---
+++ scripts/bar.groovy
@@ -0,0 +1,6 @@
+
+println "xxxx = " + xxxx
+println "yyyy = " + yyyy
+println "type of yyyy = " + yyyy.getClass().getSimpleName()
+
+return 3.2;
---
+++ scripts/foo.groovy
@@ -0,0 +1,10 @@
+
+if (group1 != group2) {
+   return 0.8
+}
+
+if (time < 1.6) {
+   return 3.2
+}
+
+return 0.8
